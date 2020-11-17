699d77a334230d68adaca6869a1d7f425ffebb13
Chonnawee <chonnawee_cha@true-e-logistics.com>
2020-11-17 12:11:51 +0700

    add missing header

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DSBridge-IOS",
-            dependencies: []),
+            dependencies: [],
+            path: "dsbridge",
+            publicHeadersPath: "."),
         .testTarget(
             name: "DSBridge-IOSTests",
             dependencies: ["DSBridge-IOS"]),
--- Tests/DSBridge-IOSTests/DSBridge_IOSTests.swift
+++ Tests/DSBridge-IOSTests/DSBridge_IOSTests.swift
@@ -6,7 +6,8 @@ final class DSBridge_IOSTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DSBridge_IOS().text, "Hello, World!")
+        let view = DWKWebView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        XCTAssertNotNil(view)
     }
 
     static var allTests = [
